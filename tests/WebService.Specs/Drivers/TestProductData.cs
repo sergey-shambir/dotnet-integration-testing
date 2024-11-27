804be177891cfa37c78d6115344425f670e371ac
Сергей Шамбир <sergei.shambir@travelline.ru>
2024-11-27 07:27:34 +0300

    Внедрил описания свойств в тесте на русском языке по совету Романа Лопатина

--- tests/WebService.Specs/Drivers/TestProductData.cs
+++ tests/WebService.Specs/Drivers/TestProductData.cs
@@ -1,8 +1,23 @@
+using Reqnroll.Assist.Attributes;
+
 namespace WebService.Specs.Drivers;
 
-public record TestProductData(
-    string Code,
-    string Description,
-    decimal Price,
-    uint StockQuantity
-);
+public class TestProductData(
+    string code,
+    string description,
+    decimal price,
+    uint stockQuantity
+)
+{
+    [TableAliases("Код")]
+    public string Code { get; init; } = code;
+
+    [TableAliases("Описание")]
+    public string Description { get; init; } = description;
+    
+    [TableAliases("Цена")]
+    public decimal Price { get; init; } = price;
+
+    [TableAliases("Количество")]
+    public uint StockQuantity { get; init; } = stockQuantity;
+}
